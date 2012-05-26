6b574d58ae699bc09573c88afca090e4b9730073
Rommel Castro A <rommelxcastro@gmail.com>
2012-05-26 16:30:08 -0600

    added audio

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function () {
+    var player = document.getElementById('countdown-audio');
     $('.open-countdown').click(function () {
-        $('#countdown-time').countDown();
+        $('#countdown-time').countDown({
+            callBack: function() {
+                player.stop();
+            }
+        });
+        player.play();
     });
 });
