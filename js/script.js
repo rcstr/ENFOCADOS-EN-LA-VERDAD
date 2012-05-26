2c12e5ee99dfc4705d708312a9f6dbd5179588fb
Rommel Castro A <rommelxcastro@gmail.com>
2012-05-26 17:23:27 -0600

    vote system

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
-    var player = document.getElementById('countdown-audio');
+    var player = document.getElementById('countdown-audio'),
+        countdownHtml = '<span id=\"countdown-time\"></span>';
     $('.open-countdown').click(function () {
         $('#countdown-time').countDown({
             callBack:function () {
@@ -13,5 +14,19 @@ $(document).ready(function () {
     $('#countdown').on('hide', function () {
         player.pause();
         player.currentTime = 0;
+        $('#countdown-time').remove();
+        $('.modal-body h1').append(countdownHtml);
     })
+
+
+    $('.add-1').click(function() {
+       var val = parseFloat($('.counter-1').text()) + 1;
+       $('.counter-1').text(val);
+    });
+
+    $('.add-2').click(function() {
+       var val = parseFloat($('.counter-2').text()) + 1;
+       $('.counter-2').text(val);
+    });
+
 });
