f03d1081ed9871c5adf56fa5322a8d7fd6e73d5e
Rommel Castro A <rommelxcastro@gmail.com>
2012-05-26 17:00:15 -0600

    editions on audio, timer

--- js/script.js
+++ js/script.js
@@ -2,10 +2,16 @@ $(document).ready(function () {
     var player = document.getElementById('countdown-audio');
     $('.open-countdown').click(function () {
         $('#countdown-time').countDown({
-            callBack: function() {
-                player.stop();
+            callBack:function () {
+                player.pause();
+                player.currentTime = 0;
             }
         });
         player.play();
     });
+
+    $('#countdown').on('hide', function () {
+        player.pause();
+        player.currentTime = 0;
+    })
 });
